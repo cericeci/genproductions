ad2d2c25b6343daeacb7f3c122490fb72cc54f35
cericeci <cericeci@cern.ch>
2021-03-11 11:33:19 +0100

    Fixed if-else

--- bin/MadGraph5_aMCatNLO/gridpack_generation.sh
+++ bin/MadGraph5_aMCatNLO/gridpack_generation.sh
@@ -84,9 +84,7 @@ make_gridpack () {
         echo "<<compute_widths X>> is used in your customizecards.dat"
         echo "This could be problematic from time to time, so instead use <<set decay wX AUTO>> for width computations. Please take a look at \"compute_widths\" under \"Troubleshooting_and_Suggestions\" section."
         echo "https://twiki.cern.ch/twiki/bin/view/CMS/QuickGuideMadGraph5aMCatNLO#Troubleshooting_and_Suggestions"
-        exit 1;
-      fi
-      else return 1;
+        exit 1; else return 1; fi
     fi
 
     # CMS Connect runs git status inside its own script.
